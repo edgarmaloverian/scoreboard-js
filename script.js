38cb62073b4b0e6625bbea0121d74ae64bc44e98
Edgar Maloverian <edgar@maloverian.com>
2022-12-27 23:36:13 +0200

    introduced Active Player state/switch

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ TBD: Configuration
 
 /* Let's initialize frame object */
 var score1 = 0
-var score2 = 2
+var score2 = 0
+var activeplayer = 1
 var frame = []
 /* move array should start with player identifier, either 1 or 2  */
 var move = []
+
+function ActivatePlayer(player) { /* 0 means change */
+    if (player == 1) {activeplayer = 1}
+    if (player == 2) {activeplayer = 2}
+    if (player == 0) {
+        if (activeplayer == 2) {activeplayer = 1}
+        else if (activeplayer == 1) {activeplayer = 2}
+    }
+document.getElementById("Activeplayer").innerHTML= activeplayer
+}
+
 
 function ResetScore(){
     score1 = 0
